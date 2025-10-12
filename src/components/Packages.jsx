480415ac187f8f6a989b4310784767841d7fef33
Dann <Lesterdannlopez7@gmail.com>
2025-10-12 16:32:03 +0800

    Removed the Undercleaning Service

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -368,7 +368,7 @@ function Packages() {
           </motion.h3>
 
           {/* Under Body Cleaning Promo */}
-          <motion.div 
+          {/* <motion.div 
             className="mb-8 flex justify-center"
             variants={containerVariants}
           >
@@ -379,9 +379,9 @@ function Packages() {
               <h4 className="text-2xl font-bold mb-4">Under Body Cleaning</h4>
               <p className="text-xl font-semibold text-red-400">prices starting at $250</p>
             </motion.div>
-          </motion.div>
+          </motion.div> */}
 
-          {/* Body Cleaning & Protection */}
+          {/* Body Cleaning & Protection
           <motion.div 
             className="mb-8"
             variants={containerVariants}
@@ -418,7 +418,7 @@ function Packages() {
                 <p className="text-3xl font-bold text-white">$1750</p>
               </motion.div>
             </motion.div>
-          </motion.div>
+          </motion.div> */}
 
           {/* Paint Correction */}
           <motion.div 
